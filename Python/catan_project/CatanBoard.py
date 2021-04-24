5ba0dc56196352fcc0bfae6112fbe1f602bd44a7
Raymart Ballesteros <raymartb40@gmail.com>
2021-04-24 16:03:04 -0700

    Catan: initial creation of catan board class

---
+++ Python/catan_project/CatanBoard.py
@@ -0,0 +1,19 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Sat Apr 24 15:51:16 2021
+
+@package: CatanBoard
+@author: Raymart Ballesteros
+@date: 4/24/2021
+@brief: This contains the classes to represent the Catan board
+"""
+
+# imports
+from CatanSettlements import CatanSettlements, Colors
+from CatanRoads import CatanRoads
+from CatanTiles import CatanTiles, ResourceTypes
+
+
+class CatanBoard:
+    def __init__(self):
+        pass
